4804306abb1270f9cb34188c25573861a1552168
violincase13 <126985262+violincase13@users.noreply.github.com>
2023-03-04 22:42:17 +0200

    Create crawler.py
    a web crawler to extract all the subpages of a website page

---
+++ crawler.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+ 
+urls = 'https://www.mai.gov.ro'
+grab = requests.get(urls)
+soup = BeautifulSoup(grab.text, 'html.parser')
+ 
+# opening a file in write mode
+f = open("linkuri.txt", "w")
+# traverse paragraphs from soup
+for link in soup.find_all("a"):
+   data = link.get('href')
+   f.write(data)
+   f.write("\n")
+ 
+f.close()
