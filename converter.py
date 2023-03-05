e2a6188dc23e2f4b3f3c4f5132a820a6007d0869
violincase13 <126985262+violincase13@users.noreply.github.com>
2023-03-05 20:54:26 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -3,5 +3,5 @@ import tabula as tabula
 # Read a PDF File
 df = tabula.read_pdf("/home/user/Desktop/crawler/pdf/pdf103.pdf", pages='all')[0]
 # convert PDF into CSV
-tabula.convert_into("/home/user/Desktop/crawler/pdf/pdf103.pdf", "/home/flavia/Desktop/crawler/csv/pdf103.csv", output_format="csv", pages='all')
+tabula.convert_into("/home/user/Desktop/crawler/pdf/pdf103.pdf", "/home/user/Desktop/crawler/csv/pdf103.csv", output_format="csv", pages='all')
 print(df)
